202fabac6eab79f28e98fd3223ce2f9f8c2f605a
Md Ariful Islam (Daud) <arifuldaud@gmail.com>
2026-09-20 10:59:12 +0000

    String

---
+++ String.py
@@ -0,0 +1,43 @@
+
+a = "hello"
+b = "world"
+c= a+b 
+print (c)
+
+
+a = "hello"
+b = "world"
+c= a + "" + b
+print (c)
+
+# Format Strings phython 
+
+age = 36 
+txt = f"My Name is Arif, i am {age}"
+print(txt)
+
+price = 54
+txt = f"The price is {price} dollars"
+print(txt)
+
+txt = f"The price is {20*40} dollars"
+print(txt)
+txt = f"The price is {20+40} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
+# Create the variable
+txt = "Hello, World!"
+
+# Print characters from index 2 to 5
+print(txt[2:5])
+
+# Print in upper case
+print(txt.upper())
+
+# Create the name variable
+name = "Python"
+
+# Print using an f-string
+print(f"I love {name}")
