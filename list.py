6009aa62e1feddb5d8819c041b36a898eb81938e
Md Ariful Islam (Daud) <arifuldaud@gmail.com>
2026-09-21 09:18:28 +0000

    add

---
+++ list.py
@@ -0,0 +1,13 @@
+list = ["apple","Banna","chery","apple","Banna","chery"]
+print(list)
+print(len(list))
+print(type(list))
+print (list[3])
+print (list[-4])
+print (list[2:5])
+print (list[:5])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
