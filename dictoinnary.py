f90e277aabfea39fc9730f8bfc80902607042ae4
Md Ariful Islam (Daud) <arifuldaud@gmail.com>
2026-09-26 09:36:17 +0000

    add dictionary

---
+++ dictoinnary.py
@@ -0,0 +1,26 @@
+
+
+person = {
+    "name": "Ariful",
+    "age": 25,
+    "city": "Dhaka"
+}
+print(person)
+print(person["name"])
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 2020,
+    "color": ["red", "blue", "yellow"]
+}
+
+print(thisdict)
+print(len(thisdict))
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
